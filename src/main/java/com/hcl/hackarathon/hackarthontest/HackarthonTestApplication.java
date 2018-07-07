19eb0675e01b7f01ee8c279597cdfdae419adf0c
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2018-07-07 20:38:12 +0530

    inintial

---
+++ src/main/java/com/hcl/hackarathon/hackarthontest/HackarthonTestApplication.java
@@ -0,0 +1,12 @@
+package com.hcl.hackarathon.hackarthontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackarthonTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackarthonTestApplication.class, args);
+	}
+}
